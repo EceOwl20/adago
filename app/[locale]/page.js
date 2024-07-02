9145358f035042dcbb697c2fddffd8c58f483287
Atribu <oguzkilinc.ant@gmail.com>
2024-07-02 15:31:56 +0300

    ikjkmji

--- app/[locale]/page.js
+++ app/[locale]/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Contactus from "./component/common/contactus.jsx"
 import EmailSection from "./component/common/emailSection.jsx"
 import Values from "./component/common/values.jsx";
+import { defaultLocale, locales } from "@/config";
 
 import HomeVilla from "./component/common/homeVilla.jsx";
 import ContactEmail from "./component/common/contactEmail.jsx";
---
+++ config.js
@@ -0,0 +1,13 @@
+export const locales = ["tr", "en","ru"];
+
+export const defaultLocale = "tr";
+
+export const localePrefix = "as-needed";
+
+export const pathnames = {
+  "/": {
+    tr: "/",
+    en: "/homepage",
+    ru: "/домашняястраница"
+  },
+};
