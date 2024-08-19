6673073a11184790f490a9fc3db8ced090934ba5
EceOwl20 <ecebayramer20@gmail.com>
2024-08-20 01:00:02 +0300

    googletag edited

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -8,6 +8,7 @@ import { notFound } from "next/navigation";
 import { NextIntlClientProvider, useMessages } from "next-intl";
 import Whatsapp from "./component/common/Whatsapp";
 import { getTranslations, unstable_setRequestLocale } from "next-intl/server";
+import { GoogleTagManager } from '@next/third-parties/google'
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -36,41 +37,11 @@ export default  function RootLayout({ children, language })  {
 
   return (
     <html lang={language}>
-      <Head>
-        {/* Google Tag Manager */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){
-              w[l]=w[l]||[];
-              w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
-              var f=d.getElementsByTagName(s)[0],
-                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-              j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-              f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-MGTLX7MW');`,
-          }}
-        />
-        {/* End Google Tag Manager */}
-      <meta charSet="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="theme-color" content="#ffffff" />
-      
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        <meta name="keywords" content={metadata.keywords}></meta>
-        
-      </Head>
+      <GoogleTagManager gtmId="GTM-MGTLX7MW" />
       <body className={inter.className}>
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-MGTLX7MW"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          ></iframe>
-        </noscript>
-        {/* End Google Tag Manager (noscript) */}
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MGTLX7MW"
+height="0" width="0" style={{display:"none", visibility:"hidden"}}></iframe></noscript>
+
       <NextIntlClientProvider messages={messages}>
         <Navbar/>
         <Whatsapp/>
