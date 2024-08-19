d936eaf83b691eac2b6e917b014bb1dfc2d703c2
EceOwl20 <ecebayramer20@gmail.com>
2024-08-19 18:29:16 +0300

    metadata added

--- app/[locale]/project/layout.js
+++ app/[locale]/project/layout.js
@@ -1,25 +1,28 @@
-import Navbar from "../component/common/Navbar";
 import "../../../app/globals.css";
-import Footer from "../component/common/Footer";
 import { defaultLocale, locales } from "@/config";
 import { notFound } from "next/navigation";
 import { NextIntlClientProvider, useMessages } from "next-intl";
 import Menubar from "../component/common/Menubar";
 
-// export async function getServerSideProps(context) {
-//   // Get the language from the request headers
-//   const language = context.req.headers['accept-language'];
-//   context.res.setHeader('Set-Cookie', `language=${language}; Path=/; HttpOnly`);
-// }
+// export const metadata = {
+//   title: "Project villa - AdaGo",
+//   description: "AdaGo Yapı İnşaat villa projeleri hakkında bilgi edinin.",
+//   keywords: "villa project, AdaGo Yapı İnşaat",
+// };
 
-export default function Layout({ children, language }) {
+export default function ProjectLayout({ children, pageTitle }) {
   if (!locales.includes(defaultLocale)) notFound();
   const messages = useMessages();
 
   return (
     <div className="">
+      {/* <Head>
+        <title>{pageTitle} - AdaGo</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="keywords" content={metadata.title} />
+      </Head> */}
       <NextIntlClientProvider messages={messages}>
-        <Menubar />
+      <Menubar />
         {children}
       </NextIntlClientProvider>
     </div>
